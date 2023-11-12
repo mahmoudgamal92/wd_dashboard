6135cda03a50ab2a547c773d35e474522b68fabf
Mahmoud Gamal <mgamal@sgs-uae.com>
2023-11-12 16:40:41 +0200

    test commit

--- WebApi/cats/list.php
+++ WebApi/cats/list.php
@@ -6,7 +6,7 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 include_once './../../dbcontext/connect.php';
-
+//$cmd = "select * from cats";
 $cmd = "select * from cats";
 $res = mysqli_query($con, $cmd);
 if(mysqli_num_rows($res) > 0)
