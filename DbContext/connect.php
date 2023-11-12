9cd0cee90068419d21ce9056a88658eed9c21514
Mahmoud Gamal <mgamal@sgs-uae.com>
2023-11-12 16:38:03 +0200

    test

--- DbContext/connect.php
+++ DbContext/connect.php
@@ -1,8 +1,14 @@
 <?php
+
 $dbHost = "localhost";
 $dbUser = "root";
 $dbPass = "";
 $dbName = "aqartech";
+
+// $dbHost = "162.241.218.91";
+// $dbUser = "zawyaonl_mahmoud";
+// $dbPass = "8+ja(_nF)$]O";
+// $dbName = "zawyaonl_aqartech";
 
 $con= new mysqli($dbHost,$dbUser,$dbPass,$dbName) 
 or die ("connection erorr".mysqli_error($con));
